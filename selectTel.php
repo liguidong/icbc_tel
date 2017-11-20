5c7c5757a857068475e6653ea2b054f60cb55e82
liguidong <liguidong94@gmail.com>
2017-11-20 22:47:19 +0800

    123
    123

--- selectTel.php
+++ selectTel.php
@@ -1,11 +1,11 @@
 <?php
-//连接数据库
+//连接数据库 分支
 function db()
 {
     $db_host = 'localhost';
     $db_name = 'icbc';
     $db_user = 'root';
-    $db_pwd = '123';
+    $db_pwd = '1234';
     $mysqli = new mysqli($db_host, $db_user, $db_pwd, $db_name);
     return $mysqli;
 }
